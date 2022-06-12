223fc54fb578bb8ec0fa1ad491d9be1d5c1d923a
manhctd20 <cmanh0603@gmail.com>
2022-06-12 13:27:30 +0700

    update

--- login/login/obj/Debug/login_Content.g.i.cs
+++ login/login/obj/Debug/login_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("anh1.jpg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("anh2.jpg")]
 
 
